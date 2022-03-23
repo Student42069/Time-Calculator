a5a7f4f966f430ab6e18d52b0d386fd2fc279f5a
Student42069 <lehackdeshackers@gmail.com>
2022-03-22 21:56:20 -0400

    modified variables names to be better to understand

--- time_calculator.py
+++ time_calculator.py
@@ -1,51 +1,52 @@
-def add_time(start, duration, weekday=""):
+def add_time(startTime, duration, weekday=""):
     weekdays = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
-    minstart = int(start.split(":")[1].split(" ")[0])
-    if start.split(" ")[1] == "PM":
-        hstart = int(start.split(":")[0]) + 12
+
+    minStart = int(startTime.split(":")[1].split(" ")[0])
+    if startTime.split(" ")[1] == "PM":
+        hourStart = int(startTime.split(":")[0]) + 12
     else:
-        hstart = int(start.split(":")[0])
+        hourStart = int(startTime.split(":")[0])
 
-    hadd = int(duration.split(":")[0])
-    minadd = int(duration.split(":")[1])
+    addHours = int(duration.split(":")[0])
+    addMins = int(duration.split(":")[1])
 
-    hf = hstart + hadd
-    minf = minstart + minadd
+    hourSum = hourStart + addHours
+    minSum = minStart + addMins
     alternate = False
-    if minf > 59:
-        hf += 1
-        minf -= 60
-        if hf == 12:
+    if minSum > 59:
+        hourSum += 1
+        minSum -= 60
+        if hourSum == 12:
             alternate = True
 
     days = 0
-    while hf >= 24 :
+    while hourSum >= 24 :
         days += 1
-        hf -= 24
+        hourSum -= 24
 
     pm = False
-    if hf > 12:
-        hf -= 12
+    if hourSum > 12:
+        hourSum -= 12
         pm = True
     if alternate == True:
         pm = not pm
-    if hf == 0:
-        hf = 12    
+    if hourSum == 0:
+        hourSum = 12    
 
-    time = ""
-    time += str(hf) + ":" + str(minf).zfill(2) + " "
+    finalTIme = ""
+    finalTIme += str(hourSum) + ":" + str(minSum).zfill(2) + " "
     if pm:
-        time += "PM"
+        finalTIme += "PM"
     else:  
-        time += "AM"
+        finalTIme += "AM"
     
     if weekday != "":
         dayf = weekdays[((weekdays.index(weekday.title()) + (days % 7)) % 7)]
-        time += ", " + dayf
+        finalTIme += ", " + dayf
     
     if days == 1:
-        time += " (next day)"
+        finalTIme += " (next day)"
     if days > 1:
-        time += " (" + str(days) + " days later)"
+        finalTIme += " (" + str(days) + " days later)"
         
-    return(time)
+    return(finalTIme)
